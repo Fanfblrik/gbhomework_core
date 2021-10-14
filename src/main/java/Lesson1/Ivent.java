edf4a63cf7e4d0ca476ea29b2c19963caa279654
Georgy <deadmaksimchik@gmail.com>
2021-10-14 14:59:30 +0300

    step 1

---
+++ src/main/java/Lesson1/Ivent.java
@@ -0,0 +1,9 @@
+package Lesson1;
+
+public class Ivent {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/Lesson1/Team.java
@@ -0,0 +1,13 @@
+package Lesson1;
+
+public class Team {
+
+    private String name = "Galileo";
+    private int vRun;
+    private int vSwim;
+    private int vCrawl;
+
+    
+
+
+}
