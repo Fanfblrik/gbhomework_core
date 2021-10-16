5ad588dc344710671c1ea92035d72a3cfe395204
Georgy <deadmaksimchik@gmail.com>
2021-10-16 17:34:10 +0300

    step 2.1

--- src/main/java/Lesson2/Summ.java
+++ src/main/java/Lesson2/Summ.java
@@ -4,12 +4,36 @@ public class Summ {
 
     public static void main(String[] args) {
 
-        
+        String[][] arr = new String[4][4];
+        arr[0][0] = "5";
+        arr[0][1] = "5";
+        arr[0][2] = "5";
+        arr[0][3] = "x";
+        arr[1][0] = "five";
 
+        try {
+            arrSum(arr);
+        }
+        catch (MyArraySizeException | ExceptionInInitializerError e){
+            System.out.println("Недопустимый размер массива");
+        }
     }
 
-    public static void arrSum (){
+    public static void arrSum (String[][] arr){
+        int s = 0;
 
+            for (int y = 0; y < arr.length; y++){
+                for (int x = 0; x < arr.length; x++){
+                    try {
+                        s = s + Integer.parseInt(arr[y][x]);
+                        throw new MyArrayDataException();
+                    } catch (MyArrayDataException | NumberFormatException e){
+                        System.out.println("Ошибка при попытке преобразования ячейки ");
+                       e.printStackTrace();
+                    }
+                }
+            }
+        System.out.println("Сумма - " + s);
     }
 
 }
