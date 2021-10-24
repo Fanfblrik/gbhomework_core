143ba0bf8afb60bf4d7e46c129024ed72a832320
Georgy <deadmaksimchik@gmail.com>
2021-10-24 17:42:54 +0300

    step 4.1

--- src/main/java/Lesson4/TelSpSl.java
+++ src/main/java/Lesson4/TelSpSl.java
@@ -1,10 +1,34 @@
 package Lesson4;
 
+import java.util.*;
+
 public class TelSpSl {
 
-    
-
     public static void main(String[] args) {
+
+        List<String> snames = Arrays.asList("Riihimäki", "Helsinki", "Lahti", "Oulu", "Turku", "Tampere",
+                "Vaasa", "Pori", "Tampere", "Moskov", "Pietari", "Rovaniemi", "Levi", "Pasila");
+        System.out.println("Исходный список:");
+        snames.forEach(System.out::println);
+
+        System.out.println("\nСводка:");
+        Map<String, Integer> wordsCount = new HashMap<>();
+        for (String s : snames) {
+            wordsCount.put(s, wordsCount.getOrDefault(s, 0) + 1);
+        }
+        wordsCount.entrySet().forEach(System.out::println);
+
+        System.out.println("\nБез повторов: ");
+        Set<String> unique = new HashSet<>(snames);
+        unique.forEach(System.out::println);
+
+        Phonebook pb = new Phonebook();
+
+        pb.add("Ivanov", "123");
+        pb.add("Petrov", "456");
+        pb.add("Petrov", "789");
+
+        System.out.println(pb.get("Petrov"));
 
     }
 
